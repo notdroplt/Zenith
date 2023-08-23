90a446241d7348ae1224c75a33970d8707b3ff60
Arthur C <117052412+notdroplt@users.noreply.github.com>
2023-08-22 22:33:15 -0300

    second time merging the same branch (#2)
    * this is going to break everything
    
    * mom can we have good working code
    
    * cleanup, added undone code again

--- src/runasm/runasm.hpp
+++ src/runasm/runasm.hpp
@@ -2,39 +2,87 @@
 #ifndef ZENITH_RUNASM_H
 #define ZENITH_RUNASM_H 1
 
-#include <vector>
-#include <unordered_map>
-#include <string>
+#include <cstdint>
 #include <sstream>
-#include <cstdint.h>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
-struct light_register {
-    std::string name;
-    uint16_t position;
-    uint8_t size;
-    bool shadowed;
-};
+namespace runasm
+{
 
-struct shadow_register {
-    std::string name;
-    std::vector<int16_t> shadowed_registers;
-    uint16_t padding;
-};
+    /**
+     * @brief defines a register that isn't shadowed, so "light strikes on it"
+     *
+     */
+    struct light_register
+    {
+        std::string name;  //!< architecture given register name
+        uint16_t position; //!< position defined on the runasm file
+        uint8_t size;      //!< bitsize
+    };
 
-using flags = std::unordered_map<std::string, uint8_t>;
-using format = std::vector<char> layout;
+    /**
+     * @brief defines a shadowed register, so it
+     *
+     */
+    struct shadow_register
+    {
+        std::string name;                        //!< architecure given register name
+        std::vector<int16_t> shadowed_registers; //!< shadowed registers, in order
+        uint16_t padding;                        // register padding size
+    };
 
-class InstructionSet {
-    std::string name;
-    std::vector<light_register> registers;
-    std::vector<shadow_register> shade_registers;
-    uint8_t width;
+    using flags = std::unordered_map<std::string, uint8_t>;
+
+    namespace HardwareController
+    {
+        struct offset
+        {
+            uint16_t index;
+            uint16_t size;
+        };
+
+        class Format
+        {
+            uint64_t element_count;
+            uint64_t instruction_bit_size;
+            std::unordered_map<char, std::vector<int>> format_bits;
+            std::unordered_map<char, std::string> aliases;
+            uint8_t immediates;
+            uint8_t registers;
+        };
+
+        enum Properties
+        {
+            bit_8 = 1 << 0,
+            bit_16 = 1 << 1,
+            bit_32 = 1 << 2,
+            bit_64 = 1 << 3,
+            bit_128 = 1 << 4,
+            bit_256 = 1 << 5,
+            bit_512 = 1 << 6,
+            Vectorized = 1 << 7,
+            Flags = 1 << 8,
+            Threading = 1 << 9,
+            SoftwareInterrupts = 1 << 10,
+            HardwareInterrupts = 1 << 11,
+            Paging = 1 << 12
+
+        };
+    }
+
+    class InstructionSet
+    {
+        std::string name;
+        std::vector<light_register> registers;
+        std::vector<shadow_register> shade_registers;
+        uint8_t width;
+
     public:
+        std::string serialize(std::string filename);
+        void get_serialized();
+    };
 
-    std::string serialize(std::string filename);
-    void serialized(std::string filename);
-
-    
-};
-
-#endif 
+}
+#endif
